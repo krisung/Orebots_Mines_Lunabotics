244f4495bbd106d3d56e6d22c39919467d20bf44
parkedjeep <95707234+parkedjeep@users.noreply.github.com>
2022-05-03 10:57:11 -0500

    Add files via upload

---
+++ Video Transmitter.py
@@ -0,0 +1,41 @@
+#This is the Python code for the robot side of the camera system
+#Run this script after the ground station script is running
+#Make sure the IP adress is correct
+
+import cv2
+import io
+import socket
+import struct
+import time
+import pickle
+import zlib
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#change IP address below to IP address of ground station/recieving computer
+#the IP address will change when WiFi network is changed
+client_socket.connect(('192.168.1.124', 8485))
+connection = client_socket.makefile('wb')
+
+cam = cv2.VideoCapture(0)
+
+cam.set(3, 320);
+cam.set(4, 240);
+
+img_counter = 0
+
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+while True:
+    ret, frame = cam.read()
+    result, frame = cv2.imencode('.jpg', frame, encode_param)
+#    data = zlib.compress(pickle.dumps(frame, 0))
+    data = pickle.dumps(frame, 0)
+    size = len(data)
+
+
+    print("{}: {}".format(img_counter, size))
+    client_socket.sendall(struct.pack(">L", size) + data)
+    img_counter += 1
+
+cam.release()
